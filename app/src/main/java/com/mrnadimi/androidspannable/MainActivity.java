d51915fb426e0d8d304d009a28d7d68be1465be8
bahram <mr.mohamadnadimi@gmail.com>
2022-04-24 10:19:41 -0400

    Update

--- app/src/main/java/com/mrnadimi/androidspannable/MainActivity.java
+++ app/src/main/java/com/mrnadimi/androidspannable/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mrnadimi.spannable.SpannableBuilder;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SpannableBuilder s = null;
     }
 }
